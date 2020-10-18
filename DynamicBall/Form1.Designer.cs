aa562c43467fcd650135e87bdd0fe16552eefc1c
Balytsky Igor <ibalyts@live.com>
2020-10-18 10:01:32 +0300

    Add project files.

---
+++ DynamicBall/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace DynamicBall
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pbBall = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.pbBall2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBall)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBall2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 553);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1303, 174);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pbBall
+            // 
+            this.pbBall.Image = ((System.Drawing.Image)(resources.GetObject("pbBall.Image")));
+            this.pbBall.Location = new System.Drawing.Point(93, 354);
+            this.pbBall.Name = "pbBall";
+            this.pbBall.Size = new System.Drawing.Size(50, 51);
+            this.pbBall.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbBall.TabIndex = 1;
+            this.pbBall.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 33;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1145, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pbBall2
+            // 
+            this.pbBall2.Image = ((System.Drawing.Image)(resources.GetObject("pbBall2.Image")));
+            this.pbBall2.Location = new System.Drawing.Point(680, 126);
+            this.pbBall2.Name = "pbBall2";
+            this.pbBall2.Size = new System.Drawing.Size(50, 51);
+            this.pbBall2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbBall2.TabIndex = 3;
+            this.pbBall2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Navy;
+            this.pictureBox3.Location = new System.Drawing.Point(651, 2);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 26);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(680, 27);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(50, 93);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1304, 730);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pbBall2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pbBall);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBall)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBall2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pbBall;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pbBall2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
---
+++ DynamicBall/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DynamicBall
+{
+    public partial class Form1 : Form
+    {
+        Vector v, a, r;
+        Vector v2, a2, r2;
+        Vector r0;
+        double m, m2;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            v = new Vector(500, -300);
+            
+            r = new Vector((int)pbBall.Left, (int)pbBall.Top);
+            m = 1;
+
+
+            v2 = new Vector();
+
+            r2 = new Vector((int)pbBall2.Left, (int)pbBall2.Top);
+            m2 = 1;
+            r0 = new Vector((int)pbBall2.Left, (int)pbBall2.Top+100);
+
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            double dt = timer1.Interval / 1000.0;
+
+            Vector g = new Vector(0, 980);
+            Vector F = m * g;
+
+            a = F / m;
+            v += a * dt;
+            r += v * dt;
+
+            pbBall.Location = new Point((int)r.X, (int)r.Y);
+
+
+
+            Vector F2 = 10 * (r0 - r2);
+
+            a2 = F2 / m2;
+            v2 += a2 * dt;
+            r2 += v2 * dt;
+
+
+            pbBall2.Location = new Point((int)r2.X, (int)r2.Y);
+            pictureBox4.Height = pbBall2.Top - pictureBox4.Top;
+        }
+    }
+}
